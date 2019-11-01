b2975fffb3d47d9cb0fed50e231402b5cf9a563d
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 00:18:32 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,7 +5,7 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create 
-    @school_class = SchoolClass.create()
+    @school_class = SchoolClass.create(school_class_params)
   end 
   
   def edit 
