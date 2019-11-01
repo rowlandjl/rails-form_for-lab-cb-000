7bf30d84903b7703c0a0e69fa699d757c0b83707
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 00:56:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,7 @@ class StudentsController < ApplicationController
     end 
     
     def create
-      
+      @student = Student.create(student_params(:first_name, :last_name))
     end 
     
     def show 
