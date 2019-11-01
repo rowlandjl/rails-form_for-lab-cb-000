73c6a92658e7462dfdca38f889cf170724f8a090
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 00:25:25 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
   
   def create 
     @school_class = SchoolClass.create(school_class_params(:title, :room_number))
-    redirect_to 
+    redirect_to school_class_path(@school_class)
   end 
   
   def show 
