365136c309ce02c5131cc6bcde9870a6a008d8e7
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 00:18:06 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,6 +14,7 @@ class SchoolClassesController < ApplicationController
   private 
   
   def school_class_params(*args)
-    params.require(:school_class)
+    params.require(:school_class).permit(*args)
+  end 
   
 end 
