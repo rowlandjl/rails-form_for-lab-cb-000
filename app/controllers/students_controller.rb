db337f5ad9e3bc49e71b3f55d2b9ecfde578b84a
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 01:09:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,6 +18,9 @@ class StudentsController < ApplicationController
     end 
     
     def update 
+      @student = Student.find(params[:id])
+      @student.update(student_params(:first_name, :last_name))
+      redirect_to student_path(@student)
     end 
     
     private 
